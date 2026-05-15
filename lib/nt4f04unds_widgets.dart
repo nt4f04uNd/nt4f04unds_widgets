9b6eba55079773f43d33369e9f0e7ab4529060d6
Abestanis <abestanis.gc@gmail.com>
2026-05-15 19:00:20 +0200

    Re-add main library directive

--- lib/nt4f04unds_widgets.dart
+++ lib/nt4f04unds_widgets.dart
@@ -3,4 +3,6 @@
 *  Licensed under the BSD-style license. See LICENSE in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
+library;
+
 export 'src/nt4f04unds_widgets.dart';
